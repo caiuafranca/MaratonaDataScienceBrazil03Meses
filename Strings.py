ed749bf1817439721ea9272a15dfcbbb16b06046
Caiua Gomes de Franca <caiua.franca@ale.com.br>
2018-12-03 17:14:01 -0300

    Ajuste no Script Python

--- Strings.py
+++ Strings.py
@@ -4,10 +4,19 @@ frase = input()
 
 print(type(frase))
 
+# Tamanho da Frase
 print(len(frase))
 
+# Lower Case
 print(frase.lower())
 
+# Quebrar String criando uma Lista
 vec = frase.split()
 
 print(vec)
+
+nome = 'Caiua'
+sobrenome = 'França'
+
+# Concatenar String
+print ("{} {} seja bem vindo ao Sistema!".format(nome, sobrenome))
